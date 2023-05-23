4cccc0404211f93ffef13baf7f6681d03860fcb2
devalseth <devalseth27@gmail.com>
2023-05-23 12:17:06 -0700

    springboot overview

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/deval/springboot/demo/myapp/rest/FunRestController.java
@@ -0,0 +1,19 @@
+package com.deval.springboot.demo.myapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${custom.fname}")
+    private String fName;
+
+    @Value("${custom.lname}")
+    private String lName;
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello worlds !" + fName + " " + lName;
+    }
+}
